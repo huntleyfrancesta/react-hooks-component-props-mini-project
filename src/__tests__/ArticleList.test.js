b088939ad56e0504cde3a0df42a7158a66e62ca3
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-05-13 23:12:19 -0400

    done

--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -1,34 +1,18 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import ArticleList from "../components/ArticleList";
+import React from "react";
+import Article from "./Article"
 
-const posts = [
-  {
-    id: 1,
-    title: "Components 101",
-    date: "December 15, 2020",
-    preview: "Setting up the building blocks of your site",
-  },
-  {
-    id: 2,
-    title: "React Data Flow",
-    date: "December 11, 2020",
-    preview: "Passing props is never passé",
-  },
-  {
-    id: 3,
-    title: "Function Components vs Class Components",
-    date: "December 10, 2020",
-    preview: "React, meet OOJS.",
-  },
-];
+function ArticleList({ posts }){
+  const articleComponents = posts.map(({ id, title, preview, date, minutes }) => {
+    return <Article
+              key={id} 
+              title={title}
+              date={date}
+              preview={preview}
+              minutes={minutes}
+           />
+  })
 
-test("renders a <main> element", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main")).toBeInTheDocument();
-});
+  return <main>{articleComponents}</main>
+}
 
-test("renders a Article component for each post passed as a prop", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main").children).toHaveLength(3);
-});
+export default ArticleList
